8ca89eeb4f6e8dbdbb98b7bcd0ab5df0ad7794a5
Aswin-art <aswinarung2@gmail.com>
2024-08-29 01:03:38 +0700

    fix: some api

--- src/channels/channel.repository.js
+++ src/channels/channel.repository.js
@@ -1,68 +1,69 @@
-const prisma = require('../db');
-const cuid = require('cuid');
+const prisma = require("../db");
+const cuid = require("cuid");
 
 const findall = async (name, user_id) => {
-    const ch = await prisma.channels.findMany({
-        where:{
-            NOT:{
-                user_id:user_id
-            },
-            name:{
-                contains: name,
-            }
-        }
-    });
-    return ch;
-}
+  const ch = await prisma.channels.findMany({
+    where: {
+      NOT: {
+        user_id: user_id,
+      },
+      name: {
+        contains: name,
+      },
+    },
+    include: {
+      users: true,
+    },
+  });
+  return ch;
+};
 
 const findbyid = async (id) => {
-    const ch = await prisma.channels.findUnique({
-        where:{
-            id:id,
-        },
-        include:{
-            users:true,
-            follows:true,
-            events:true,
-        }
-    })
-    return ch;
-}
+  const ch = await prisma.channels.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      users: true,
+      follows: true,
+      events: true,
+    },
+  });
+  return ch;
+};
 
 const insert = async (channelsdata) => {
-    const ch = await prisma.channels.create({
-        data:{
-            id: cuid(),
-            user_id:channelsdata.user_id,
-            name:channelsdata.name,
-            description:channelsdata.description,
-            image:channelsdata.image,
-            no_rek: channelsdata.no_rek,
-            nik: channelsdata.nik,
-            ktp: channelsdata.ktp,
-        }
-    })
-    return ch;
-}
+  const ch = await prisma.channels.create({
+    data: {
+      id: cuid(),
+      user_id: channelsdata.user_id,
+      name: channelsdata.name,
+      description: channelsdata.description,
+      image: channelsdata.image,
+      //   no_rek: channelsdata.no_rek,
+      //   nik: channelsdata.nik,
+    },
+  });
+  return ch;
+};
 
 const edit = async (id, channelsdata) => {
-    const ch = await prisma.channels.update({
-        where:{
-            id: id,
-        },
-        data:{
-            name:channelsdata.name,
-            description:channelsdata.description,
-            image:channelsdata.image,
-        }
-    })
-    return ch;
-}
+  const ch = await prisma.channels.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name: channelsdata.name,
+      description: channelsdata.description,
+      image: channelsdata.image,
+    },
+  });
+  return ch;
+};
 
 module.exports = {
-    findall,
-    findbyid,
-    insert,
-    edit,
-}
-
+  findall,
+  findbyid,
+  insert,
+  edit,
+};
