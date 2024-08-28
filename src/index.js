40236eba91bd7ffc8aec5d374251e3a00fe3d48e
Aswin-art <aswinarung2@gmail.com>
2024-08-28 23:22:16 +0700

    feat: add cors package & change db schema

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 const upload = require("./middleware/multer");
 const middlewareLogRequest = require("./middleware/logs");
 
+app.use(cors());
 app.use(express.json());
 app.use(middlewareLogRequest);
 app.use(express.urlencoded({ extended: true }));
