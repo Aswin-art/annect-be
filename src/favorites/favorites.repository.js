b197007781d1291dfec29ae54cd1bd4769f9ef6d
Atma1 <atmasusanto08@gmail.com>
2024-09-04 20:18:13 +0800

    add getById

--- src/favorites/favorites.repository.js
+++ src/favorites/favorites.repository.js
@@ -17,6 +17,15 @@ const insert = (favoriteData) =>
     },
   });
 
+const getById = (user_id, event_id) => (
+  prisma.favorites.findMany({
+    where: {
+      user_id: user_id,
+      event_id: event_id
+    }
+  })
+)
+
 const deleteById = (favorite_id) =>
   prisma.favorites.delete({
     where: {
@@ -28,4 +37,5 @@ module.exports = {
   getAll,
   deleteById,
   insert,
+  getById,
 };
