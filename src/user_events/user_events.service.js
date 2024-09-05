5d32b8752ef2d50564b4c95a858584eb60c1726c
Atma1 <atmasusanto08@gmail.com>
2024-09-05 21:35:02 +0800

    validate incoming input before inserting to user_event table

--- src/user_events/user_events.service.js
+++ src/user_events/user_events.service.js
@@ -1,8 +1,13 @@
 const { getById, insert } = require("./user_events.repository");
+const userEventsInsertSchema = require("../schema/userEvents");
 
 const getUserEventById = (user_id) => getById(user_id);
 
-const inserUserEvent = (user_id, event_id) => insert(user_id, event_id);
+const inserUserEvent = (user_event_data) => {
+  userEventsInsertSchema.parse(user_event_data);
+  const { user_id, event_id } = user_event_data;
+  return insert(user_id, event_id);
+};
 
 module.exports = {
   getUserEventById,
