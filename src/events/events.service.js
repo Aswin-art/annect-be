0dbaa4f953b01695a6ac31772dcfcfb52a95d957
xGfrost <fadelakbar528@gmail.com>
2024-09-06 22:06:38 +0700

    add check if event exist and add if_join property

--- src/events/events.service.js
+++ src/events/events.service.js
@@ -17,15 +17,37 @@ const insertEvent = async (eventData) => {
 const getEventDetail = async (event_id, user_id) => {
   const eventDetail = await getDetail(event_id);
 
+  if (!eventDetail) {
+    throw Error("event not found!")
+  }
+  
+
   eventDetail.is_favorite = false;
+  eventDetail.is_join = false;
+  
+  
+  
 
   if (
     eventDetail.favorites &&
     eventDetail.favorites.length > 0 &&
     user_id != null
   ) {
+    console.log(true);
+    
     eventDetail.is_favorite = eventDetail.favorites.some(
       (favorite) => favorite.user_id === user_id
+    );
+  }
+
+  if (
+    eventDetail.user_events &&
+    eventDetail.user_events.length > 0 &&
+    user_id != null
+  ) {
+    
+    eventDetail.is_join = eventDetail.user_events.some(
+      (user_events) => user_events.user_id == String(user_id)
     );
   }
 
