a102ea787b46dcd329887fa7fb6e6625ab819afc
Atma1 <atmasusanto08@gmail.com>
2024-09-06 13:45:28 +0800

    change get event filter to retreive all event based on status

--- src/events/events.controller.js
+++ src/events/events.controller.js
@@ -12,6 +12,7 @@ router.get("/", async (req, res) => {
     // Ambil parameter query dari URL
     const name = req.query.name || null;
     const user_id = req.query.users || null;
+    const status = req.quer.status || "ONGOING";
     console.log("user_id", user_id);
     const is_paid = req.query.is_paid
       ? req.query.is_paid === "true"
@@ -23,7 +24,9 @@ router.get("/", async (req, res) => {
 
     // Membuat objek filter berdasarkan parameter yang diterima
     const filter = {
-      where: {},
+      where: {
+        status: status
+      },
       orderBy: {
         created_at: 'desc' 
       },
